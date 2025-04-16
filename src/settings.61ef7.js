31ea227fd0cfda2a8cf38d0b28158fe2992970be
lipan <670339228@qq.com>
2025-04-16 12:24:00 +0800

    提交代码

---
+++ src/settings.61ef7.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/StartScene/StartScene.fire",orientation:"",jsList:["assets/Lib/ii.d8cf2.js"],bundleVers:{internal:"d0832",Solitaire:"f1b3f",StartBundle:"bd137",main:"03a3f"}};
